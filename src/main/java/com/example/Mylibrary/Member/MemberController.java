ab666e8763c9f74fa38edbe4f060d52bff93a39e
cochae <ycw1203@naver.com>
2025-04-04 00:09:03 +0900

    파일선택 없이도 업로드 가능하게 변경

--- src/main/java/com/example/Mylibrary/Member/MemberController.java
+++ src/main/java/com/example/Mylibrary/Member/MemberController.java
@@ -1,7 +1,11 @@
 package com.example.Mylibrary.Member;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -14,15 +18,26 @@ public class MemberController {
 
     @PostMapping("/addmember")
     public String addMember(@ModelAttribute Member member){
+
         var hash = new BCryptPasswordEncoder().encode(member.getPassword());
         member.setPassword(hash);
         memberRespository.save(member);
+        memberRespository.flush();
+
         return "redirect:/login.html";
     }
     @GetMapping("/register")
-    public String register(){
-        return "register.html";
+    public String register(HttpServletRequest request, HttpServletResponse response) {
+        var auth = SecurityContextHolder.getContext().getAuthentication();
+
+        // 로그인된 상태라면 자동 로그아웃
+        if (auth != null && auth.isAuthenticated()) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+
+        return "register.html"; // 회원가입 페이지 반환
     }
+
 
     @GetMapping("/login")
     public String login(){
--- src/main/java/com/example/Mylibrary/Member/MemberRespository.java
+++ src/main/java/com/example/Mylibrary/Member/MemberRespository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRespository extends JpaRepository<Member, Integer> {
-    Optional<Member> findByDisplayname(String displayname);
+    Optional<Member> findByUsername(String username);
 }
